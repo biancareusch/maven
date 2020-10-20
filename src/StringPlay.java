5d857cace98270c626350675c95630e88db679c8
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-20 13:50:45 -0500

    finished exercise

--- src/StringPlay.java
+++ src/StringPlay.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 public class StringPlay {
 
@@ -7,8 +9,15 @@ public static String capitalizeLetter(String letter){
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(capitalizeLetter("q"));
+        System.out.println("Please enter a string ");
         String letter = scanner.nextLine();
         System.out.println(capitalizeLetter(letter));
+        if(StringUtils.isNumeric(letter)){
+            System.out.println("\"" + letter + "\" is a number.");
+        }else{
+            System.out.println("\"" + letter + "\" is not a number.");
+        }
+        System.out.println("Flipped Case = " + StringUtils.swapCase(letter));
+        System.out.println("Reversed = " + StringUtils.reverse(letter));
     }
 }
